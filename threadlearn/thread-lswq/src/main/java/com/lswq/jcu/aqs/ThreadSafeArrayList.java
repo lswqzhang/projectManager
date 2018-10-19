9956ecaf95c39f92df6263b4d4eb6454c6bbfaa8
zhangshaowei03 <zhangshaowei03@meituan.com>
2018-10-19 18:49:44 +0800

    jcu

--- threadlearn/thread-lswq/src/main/java/com/lswq/jcu/aqs/ThreadSafeArrayList.java
+++ threadlearn/thread-lswq/src/main/java/com/lswq/jcu/aqs/ThreadSafeArrayList.java
@@ -6,7 +6,7 @@ public class ThreadSafeArrayList {
 
 
     public static void main(String[] args) throws InterruptedException {
-        final RunnableTest<String> lockExample = new RunnableTest(0);
+        final RunnableTest lockExample = new RunnableTest(0);
         Thread t2 = new Thread(lockExample, "lockingThread");
         t2.start();
         t2.join();
