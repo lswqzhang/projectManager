c8b453f9985645e60e9bca762d5a31238eac007d
zhangshaowei03 <zhangshaowei03@meituan.com>
2018-10-19 18:45:07 +0800

    jcu

--- threadlearn/thread-lswq/src/main/java/com/lswq/jcu/aqs/entity/RunnableTest.java
+++ threadlearn/thread-lswq/src/main/java/com/lswq/jcu/aqs/entity/RunnableTest.java
@@ -1,27 +1,42 @@
 package com.lswq.jcu.aqs.entity;
 
-import com.lswq.jcu.aqs.ThreadSafeArrayList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 线程
  *
  * @author zhangshaowei
  */
-public class RunnableTest implements Runnable {
+public class RunnableTest<E> implements Runnable {
 
-    final ThreadSafeArrayList<String> lockExample;
+    private final Lock lock = new ReentrantLock();
 
-    private int i;
+    private final List<String> list = new ArrayList<>();
 
-    public RunnableTest(ThreadSafeArrayList<String> lockExample, int i) {
+    private static Integer i = 0;
+
+    public RunnableTest(Integer i) {
         this.i = i;
-        this.lockExample = lockExample;
     }
 
     @Override
     public void run() {
         while (i < 6) {
-            lockExample.set(String.valueOf(i));
+            lock.lock();
+            lock.lock();
+            lock.lock();
+            try {
+                i++;
+                list.add(String.valueOf(i));
+                System.out.println("Adding element by thread " + Thread.currentThread().getName());
+            } finally {
+                lock.unlock();
+                lock.unlock();
+                lock.unlock();
+            }
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
