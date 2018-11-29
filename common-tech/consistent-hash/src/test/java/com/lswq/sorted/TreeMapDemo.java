1f98fb5608916e6bd69f7d034337c8f9f6cfa170
zhangshaowei03 <zhangshaowei03@meituan.com>
2018-11-29 19:06:44 +0800

    map

--- common-tech/consistent-hash/src/test/java/com/lswq/sorted/TreeMapDemo.java
+++ common-tech/consistent-hash/src/test/java/com/lswq/sorted/TreeMapDemo.java
@@ -1,11 +1,15 @@
 package com.lswq.sorted;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class TreeMapDemo {
 
-    public static void main(String[] args) {
+    @Test
+    public void treeMapTest() {
         // creating maps
         TreeMap<Integer, String> treemap = new TreeMap<Integer, String>();
 
@@ -20,6 +24,54 @@ public class TreeMapDemo {
         System.out.println("Getting tail map");
         SortedMap<Integer, String> treemapincl = treemap.tailMap(3);
         System.out.println("Tail map values: " + treemapincl);
+    }
+
+
+    @Test
+    public void mapTest() {
+        // creating maps
+        Map<Integer, String> map = new HashMap<>();
+
+
+        // populating tree map
+        for (int i = 0; i < 1001; i++) {
+            map.put(i, "two");
+        }
+
+        System.err.println(map.size());
+
+
+        System.out.println("Getting tail map");
+    }
+
+
+    @Test
+    public void skipListTest() {
+        ConcurrentSkipListMap<Integer, String> skipList = new ConcurrentSkipListMap<>();
+        skipList.put(1, "aa");
+        skipList.put(2, "bb");
+        skipList.put(3, "cc");
+        skipList.put(4, "dd");
+        skipList.put(5, "ee");
+        skipList.put(6, "ff");
+        skipList.put(7, "aa");
+        skipList.put(8, "bb");
+        skipList.put(9, "cc");
+        skipList.put(10, "dd");
+        skipList.put(11, "ee");
+        skipList.put(12, "ff");
+        skipList.put(13, "dd");
+        skipList.put(14, "ee");
+        skipList.put(15, "ff");
+        skipList.put(16, "dd");
+        skipList.put(17, "ee");
+        skipList.put(18, "ff");
+        skipList.put(19, "ff");
+        ConcurrentNavigableMap<Integer, String> integerStringConcurrentNavigableMap = skipList.tailMap(5);
+        Iterator<Integer> iterator = integerStringConcurrentNavigableMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            System.err.println(iterator.next());
+        }
     }
 
 }
--- threadlearn/thread-lswq/src/test/java/com/lswq/AppTest.java
+++ threadlearn/thread-lswq/src/test/java/com/lswq/AppTest.java
@@ -4,35 +4,53 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws InterruptedException {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 8, 100L, TimeUnit.MINUTES,
+                new LinkedBlockingDeque<>(10),
+                (r, e) -> {
+
+                });
+        for (int i = 0; i < 19; i++) {
+            executor.submit(() -> {
+                try {
+                    TimeUnit.HOURS.sleep(1L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        TimeUnit.SECONDS.sleep(10L);
+
+        System.err.println(executor.toString());
+
+        Thread.currentThread().join();
     }
 }
