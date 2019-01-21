ca680c68ef63c04298b3776207c457201872a6e9
zhangshaowei03 <zhangshaowei03@meituan.com>
2019-01-21 14:12:40 +0800

    mybatis

--- common-tech/common-database/src/main/java/com/lswq/db/mybatis/App.java
+++ common-tech/common-database/src/main/java/com/lswq/db/mybatis/App.java
@@ -1,24 +1,34 @@
 package com.lswq.db.mybatis;
 
 
+import com.lswq.db.mybatis.mapper.UserMapper;
 import com.lswq.db.mybatis.mode.Fruits;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        String resources = "SqlMapConfig.xml";
-        InputStream inpputStream = Resources.getResourceAsStream(resources);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inpputStream);
+
+    public void userInterfaceQuery() {
+        String resources = "SqlMapConfigInterface.xml";
+        InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(resources);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        Fruits u = sqlSession.selectOne("test.findUserById", 1);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Fruits u = mapper.findUserById(1);
         System.err.println(u);
         sqlSession.close();
+    }
 
+    public void userMyBatisSelect() {
+        String resources = "SqlMapConfigMapperFile.xml";
+        InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(resources);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Fruits u = sqlSession.selectOne("com.lswq.db.mybatis.mapper.UserMapper.findUserById", 1);
+        System.err.println(u);
+        sqlSession.close();
     }
 }
